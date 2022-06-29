6853a93d2eaea81339c2b8ec9ae622614e530d1d
wanda <wandaul16@gmail.com>
2022-06-29 09:44:54 +0700

    add query client

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,19 @@
 import { ChakraProvider, theme } from "@chakra-ui/react";
 import { AppProps } from "next/app";
+import {QueryClient, QueryClientProvider} from "react-query";
+import {ReactQueryDevtools} from 'react-query/devtools';
+
+const queryClient = new QueryClient();
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider resetCSS={true} theme={theme}>
+    <QueryClientProvider client={queryClient}> 
+       <ChakraProvider resetCSS={true} theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
+    <ReactQueryDevtools />
+    </QueryClientProvider>
+ 
   );
 }
 
